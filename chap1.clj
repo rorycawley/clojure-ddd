1524f9fb2390cf8c7db1ce14c7186529dd8681e3
Rory Cawley <rory.cawley@gmail.com>
2023-05-15 18:53:36 +0000

    chap 1

---
+++ chap1.clj
@@ -0,0 +1,33 @@
+; namespaces are used to group related functions
+(ns clojure-ddd.chap1))
+
+; primatives
+5
+-5
+3.14
+true
+false
+\a ; character a
+\n ; newline
+\b ; backspace
+\r ; character r
+\" ; double quote
+\t ; tab"
+- ; minus
++ ; plus
+"Rory" ; string
+'hello' ; symbol
+:good ; keyword
+nil ; null
+(+ 1 1)
+
+
+; naming a variable, cannot be modified
+(def wife "Penny")
+wife ; "Penny"
+
+; list (their contents cannot be modified)
+'(1 2)
+(list 1 2)
+(def my-list '(1 2))
+my-list ; (1 2)
